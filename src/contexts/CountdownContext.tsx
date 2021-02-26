b9cc7aaeb88df1ed1d815dd705ca697992267d95
Vando dos reis <vandodosreis2001@gmail.com>
2021-02-26 20:28:44 -0300

    Mudando o tempo

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -22,7 +22,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
 
   const { startNewChallenges } = useContext(ChallengeContext);
 
-  const [time, setTime] = useState(25 * 60);
+  const [time, setTime] = useState(0.1 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
